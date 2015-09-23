50ba9bf5dd961fe93f793ccd46f285a177dad04e
yorkie <yorkiefixer@gmail.com>
2015-09-23 16:17:40 +0800

    init repo

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # esprima-to-value
+
 convert an esprima ast object/literal to an real object
+
+### Install
+
+```sh
+$ npm install esprima-to-value --save
+```
+
+### Usage
+
+```js
+const esprima = require('esprima');
+const toValue = require('esprima-to-value');
+
+const ast = esprima.parse(code);
+console.log(ast.body);
+/**
+ * [
+ *   { type: 'Literal', value: 10 },
+ *   { type: 'ObjectExpression', properties: ... },
+ *   { type: 'ArrayExpression', elements: ... },
+ *   { type: 'FunctionExpression', body: { type: 'ReturnStatement', ... } },
+ *  ]
+ */
+console.log(toValue(ast.body));
+/**
+ * will output:
+ * [ 10, { ... }, [ ... ], 'res.body' ]
+ */
+```
+
+### License
+
+MIT
---
+++ index.js
@@ -0,0 +1,41 @@
+
+const getObject = require('esprima-get-object');
+
+function toValue (tree) {
+  if (Array.isArray(tree)) {
+    return tree.map(toValue);
+  } else {
+    let ret = undefined;
+    switch (tree.type) {
+      case 'Literal': 
+        ret = tree.value; 
+        break;
+      case 'ArrayExpression': 
+        ret = tree.elements.map(toValue); 
+        break;
+      case 'FunctionExpression':
+        ret = getReturn(tree.body.body);
+        break;
+      case 'ObjectExpression':
+        ret = {};
+        for (let prop of tree.properties) {
+          ret[prop.key.name] = toValue(prop.value);
+        }
+        break;
+    }
+    return ret;
+  }
+}
+
+function getReturn (body) {
+  const tree = body.shift();
+  if (tree.type === 'ReturnStatement') {
+    return getObject(tree.argument);
+  } else if (tree.type === 'TryStatement') {
+    return getReturn(tree.block.body);
+  } else {
+    return getReturn(body);
+  }
+}
+
+module.exports = toValue;
